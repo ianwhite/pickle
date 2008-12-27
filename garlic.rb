3a186c43c0a7bc9b4e82671cb6f07bc302f8620f
Ian White <ian.w.white@gmail.com>
2008-12-27 11:39:24 +1100

    Adding machinist to garlic build

--- garlic.rb
+++ garlic.rb
@@ -3,7 +3,8 @@ garlic do
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
   repo 'factory_girl', :url => 'git://github.com/thoughtbot/factory_girl'
-  repo 'cucumber', :url => 'git://github.com/aslakhellesoy/cucumber.git'
+  repo 'machinist', :url => 'git://github.com/notahat/machinist'
+  repo 'cucumber', :url => 'git://github.com/aslakhellesoy/cucumber'
   repo 'pickle', :path => '.'
 
   ['origin/2-1-stable', 'origin/2-2-stable'].each do |rails|
@@ -19,6 +20,7 @@ garlic do
         plugin 'cucumber' do
           `script/generate cucumber -f`
         end
+        plugin 'machinist'
       end
   
       run do
