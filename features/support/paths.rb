8409d7d18143c33d468ae929e83146c514a7eb0f
Ian White <ian.w.white@gmail.com>
2009-02-13 11:12:41 +0000

    Make pickle's features paths/rb the same as the generated paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,31 +1,38 @@
-def path_to(page_name)
-  case page_name
-  
-  when /the homepage/i
-    root_path
-  
-  # Add more page name => path mappings here
-  
-  # added by script/generate pickle path
-  
-  when /^#{capture_model}(?:'s)? page$/                           # eg. the forum's page
-    path_to_pickle $1
+module NavigationHelpers
+  def path_to(page_name)
+    case page_name
     
-  when /^#{capture_model}(?:'s)? #{capture_model}(?:'s)? page$/   # eg. the forum's post's page
-    path_to_pickle $1, $2
+    when /the homepage/i
+      root_path
+    
+    # Add more page name => path mappings here
+    
+    # added by script/generate pickle path
 
-  when /^#{capture_model}(?:'s)? #{capture_model}'s (.+?) page$/  # eg. the forum's post's comments page
-    path_to_pickle $1, $2, :extra => $3                           #  or the forum's post's edit page
+    when /^#{capture_model}(?:'s)? page$/                           # eg. the forum's page
+      path_to_pickle $1
 
-  when /^#{capture_model}(?:'s)? (.+?) page$/                     # eg. the forum's posts page
-    path_to_pickle $1, :extra => $2                               #  or the forum's edit page
+    when /^#{capture_model}(?:'s)? #{capture_model}(?:'s)? page$/   # eg. the forum's post's page
+      path_to_pickle $1, $2
 
-  when /^the (.+?) page$/                                         # translate to named route
-    send "#{$1.downcase.gsub(' ','_')}_path"
+    when /^#{capture_model}(?:'s)? #{capture_model}'s (.+?) page$/  # eg. the forum's post's comments page
+      path_to_pickle $1, $2, :extra => $3                           #  or the forum's post's edit page
+
+    when /^#{capture_model}(?:'s)? (.+?) page$/                     # eg. the forum's posts page
+      path_to_pickle $1, :extra => $2                               #  or the forum's edit page
+
+    when /^the (.+?) page$/                                         # translate to named route
+      send "#{$1.downcase.gsub(' ','_')}_path"
   
-  # end added by pickle path
+    # end added by pickle path
 
-  else
-    raise "Can't find mapping from \"#{page_name}\" to a path."
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path."
+    end
   end
+end
+
+World do |world|
+  world.extend NavigationHelpers
+  world
 end
