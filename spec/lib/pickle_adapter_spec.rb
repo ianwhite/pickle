d7f73ed1d23ee3c91ef97c16fdb4013db33a6c28
Ian White <ian.w.white@gmail.com>
2009-02-22 18:30:55 +0000

    Added specs for how machinist adapter should now behave (since machinist introduced multiple blueprints per model)

--- spec/lib/pickle_adapter_spec.rb
+++ spec/lib/pickle_adapter_spec.rb
@@ -38,18 +38,19 @@ describe Pickle::Adapter do
     end
   end
   
-  describe '::ActiveRecord' do
+  describe "adapters: " do
     before do
-      # set up a fake object space
-      @klass1 = mock('One', :name => 'One')
-      @klass2 = mock('One::Two', :name => 'One::Two')
-      Pickle::Adapter::ActiveRecord.stub!(:model_classes).and_return([@klass1, @klass2])
+      @klass1 = returning(Class.new(ActiveRecord::Base)) {|k| k.stub!(:name).and_return('One')}
+      @klass2 = returning(Class.new(ActiveRecord::Base)) {|k| k.stub!(:name).and_return('One::Two')}
+      @klass3 = returning(Class.new(ActiveRecord::Base)) {|k| k.stub!(:name).and_return('Three')}
+      Pickle::Adapter::ActiveRecord.stub!(:model_classes).and_return([@klass1, @klass2, @klass3])
     end
     
-    describe ".factories" do
-      it "should create one for each active record class" do
+    describe 'ActiveRecord' do    
+      it ".factories should create one for each active record class" do
         Pickle::Adapter::ActiveRecord.should_receive(:new).with(@klass1).once
         Pickle::Adapter::ActiveRecord.should_receive(:new).with(@klass2).once
+        Pickle::Adapter::ActiveRecord.should_receive(:new).with(@klass3).once
         Pickle::Adapter::ActiveRecord.factories
       end
       
@@ -57,100 +58,93 @@ describe Pickle::Adapter do
         before do
           @factory = Pickle::Adapter::ActiveRecord.new(@klass2)
         end
-        
+      
         it "should have underscored (s/_) name of Class as #name" do
           @factory.name.should == 'one_two'
         end
-        
+      
         it "#create(attrs) should call Class.create!(attrs)" do
           @klass2.should_receive(:create!).with({:key => "val"})
           @factory.create(:key => "val")
         end
       end
     end
-  end
   
-  describe '::FactoryGirl' do
-    before do
-      # set up a fake object space
-      @factory1 = mock('factory1', :factory_name => :one, :build_class => (@class1 = mock('Class1')))
-      @factory2 = mock('factory2', :factory_name => :two, :build_class => (@class2 = mock('Class2')))
-      Factory.stub!(:factories).and_return(:factory1 => @factory1, :factory2 => @factory2)
-    end
+    describe 'FactoryGirl' do
+      before do
+        Factory.factories = {}
+        Factory.define(:one, :class => @klass1) {}
+        Factory.define(:two, :class => @klass2) {}
+        @factory1 = Factory.factories[:one]
+        @factory2 = Factory.factories[:two]
+      end
     
-    describe ".factories" do
-      it "should create one for each factory" do
+      it ".factories should create one for each factory" do
         Pickle::Adapter::FactoryGirl.should_receive(:new).with(@factory1).once
         Pickle::Adapter::FactoryGirl.should_receive(:new).with(@factory2).once
         Pickle::Adapter::FactoryGirl.factories
       end
-      
+    
       describe ".new(factory)" do
         before do
           @factory = Pickle::Adapter::FactoryGirl.new(@factory1)
         end
-        
+      
         it "should have name of factory_name" do
           @factory.name.should == 'one'
         end
-        
+      
         it "should have klass of build_class" do
-          @factory.klass.should == @class1
+          @factory.klass.should == @klass1
         end
-        
+      
         it "#create(attrs) should call Factory(<:key>, attrs)" do
           Factory.should_receive(:create).with("one", {:key => "val"})
           @factory.create(:key => "val")
         end
       end
     end
-  end
   
-  describe '::Machinist' do
-    before do
-      # set up a fake object space
-      @klass1 = mock('One', :name => 'One', :make => true, :make_unsaved => true)
-      @klass1.instance_variable_set('@blueprint', true)
-      @klass2 = mock('Two', :name => 'Two')
-      @klass3 = mock('Two::Sub', :name => 'Two::Sub', :make_special => true, :make => true, :make_unsaved => true)
-      @klass3.instance_variable_set('@blueprint', true)
-      Pickle::Adapter::Machinist.stub!(:model_classes).and_return([@klass1, @klass2, @klass3])
-    end
-    
-    describe ".factories" do
-      it "should create one for each machinist make method, except make_unsaved" do
-        Pickle::Adapter::Machinist.should_receive(:new).with(@klass1, 'make').once
-        Pickle::Adapter::Machinist.should_receive(:new).with(@klass3, 'make').once
-        Pickle::Adapter::Machinist.should_receive(:new).with(@klass3, 'make_special').once
-        Pickle::Adapter::Machinist.factories
+    describe 'Machinist' do
+      before do
+        @klass1.blueprint {}
+        @klass3.blueprint {}
+        @klass3.blueprint(:special) {}
       end
       
-      describe ".new(Class, 'make')" do
+      it ".factories should create one for each master blueprint, and special case" do
+        Pickle::Adapter::Machinist.should_receive(:new).with(@klass1, :master).once
+        Pickle::Adapter::Machinist.should_receive(:new).with(@klass3, :master).once
+        Pickle::Adapter::Machinist.should_receive(:new).with(@klass3, :special).once
+        Pickle::Adapter::Machinist.factories
+      end
+        
+      describe ".new(Class, :master)" do
         before do
-          @factory = Pickle::Adapter::Machinist.new(@klass1, 'make')
+          @factory = Pickle::Adapter::Machinist.new(@klass1, :master)
         end
         
         it "should have underscored (s/_) name of Class as #name" do
           @factory.name.should == 'one'
         end
         
-        it "#create(attrs) should call Class.make(attrs)" do
-          @klass1.should_receive(:make).with({:key => "val"})
+        it "#create(attrs) should call Class.make(:master, attrs)" do
+          @klass1.should_receive(:make).with(:master, {:key => "val"})
           @factory.create(:key => "val")
         end
       end
       
-      describe ".new(Class, 'make_special')" do
+      describe ".new(Class, :special)" do
         before do
-          @factory = Pickle::Adapter::Machinist.new(@klass3, 'make_special')
+          @factory = Pickle::Adapter::Machinist.new(@klass3, :special)
         end
         
         it "should have 'special_<Class name>' as #name" do
-          @factory.name.should == 'special_two_sub'
+          @factory.name.should == 'special_three'
         end
         
-        it "#create(attrs) should call Class.make_special(attrs)" do
-          @klass3.should_receive(:make_special).with({:key => "val"})
+        it "#create(attrs) should call Class.make(:special, attrs)" do
+          @klass3.should_receive(:make).with(:special, {:key => "val"})
           @factory.create(:key => "val")
         end
       end
