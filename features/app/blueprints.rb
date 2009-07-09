b524e1540319444f511c9acff9968a77cbb41535
Ian White <ian.w.white@gmail.com>
2009-07-09 10:27:14 +0100

    Update machinist in pickle cucumber tests for latest machinist

--- features/app/blueprints.rb
+++ features/app/blueprints.rb
@@ -1,8 +1,11 @@
 # Blueprints
-require 'machinist'
+require 'machinist/active_record'
 
 Sham.spoon_name { |i| "Spoon #{i}" }
 
 Spoon.blueprint do
   name { Sham.spoon_name }
 end
+
+# reset shams between scenarios
+Before { Sham.reset }
