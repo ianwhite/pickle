c8a11914ea948349da25de389fa50a0b0d6575ab
Ian White <ian.w.white@gmail.com>
2009-07-09 10:26:47 +0100

    Use actual webrat in pickle cucumber tests, update env for latest cucumber

--- features/app/faux_webrat.rb
+++
@@ -1,5 +0,0 @@
-# just simulate the webrat functionality we need for pages_steps
-
-def visit(path)
-  get path
-end
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,10 +2,20 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '../../../../../../config/environment')
 require 'cucumber/rails/world'
+
+require 'cucumber/formatter/unicode'
+
 Cucumber::Rails.use_transactional_fixtures
+
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
 
 # Comment out the next line if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
+require 'webrat/core/matchers'
 
 # Pickle
 require 'pickle/world'
@@ -22,7 +32,3 @@ __APP__ = File.expand_path(File.join(File.dirname(__FILE__), '../app'))
 require "#{__APP__}/app"
 require "#{__APP__}/factories"
 require "#{__APP__}/blueprints"
-require "#{__APP__}/faux_webrat"
-
-# reset shams between scenarios
-Before { Sham.reset }
