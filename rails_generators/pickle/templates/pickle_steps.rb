2d3e8d125186e23054094a53efe7c4e4c9a7ed28
Ian White <ian.w.white@gmail.com>
2009-11-24 19:19:17 +0000

    Fail faster in steps when pickle ref not found by using model! in more places

--- rails_generators/pickle/templates/pickle_steps.rb
+++ rails_generators/pickle/templates/pickle_steps.rb
@@ -18,7 +18,7 @@ end
 
 # find a model
 Then(/^#{capture_model} should exist(?: with #{capture_fields})?$/) do |name, fields|
-  find_model(name, fields).should_not be_nil
+  find_model!(name, fields)
 end
 
 # not find a model
@@ -44,30 +44,30 @@ end
 
 # assert model is in another model's has_many assoc
 Then(/^#{capture_model} should be (?:in|one of|amongst) #{capture_model}(?:'s)? (\w+)$/) do |target, owner, association|
-  model(owner).send(association).should include(model(target))
+  model!(owner).send(association).should include(model!(target))
 end
 
 # assert model is not in another model's has_many assoc
 Then(/^#{capture_model} should not be (?:in|one of|amongst) #{capture_model}(?:'s)? (\w+)$/) do |target, owner, association|
-  model(owner).send(association).should_not include(model(target))
+  model!(owner).send(association).should_not include(model!(target))
 end
 
 # assert model is another model's has_one/belongs_to assoc
 Then(/^#{capture_model} should be #{capture_model}(?:'s)? (\w+)$/) do |target, owner, association|
-  model(owner).send(association).should == model(target)
+  model!(owner).send(association).should == model!(target)
 end
 
 # assert model is not another model's has_one/belongs_to assoc
 Then(/^#{capture_model} should not be #{capture_model}(?:'s)? (\w+)$/) do |target, owner, association|
-  model(owner).send(association).should_not == model(target)
+  model!(owner).send(association).should_not == model!(target)
 end
 
 # assert model.predicate? 
 Then(/^#{capture_model} should (?:be|have) (?:an? )?#{capture_predicate}$/) do |name, predicate|
-  model(name).should send("be_#{predicate.gsub(' ', '_')}")
+  model!(name).should send("be_#{predicate.gsub(' ', '_')}")
 end
 
 # assert not model.predicate?
 Then(/^#{capture_model} should not (?:be|have) (?:an? )?#{capture_predicate}$/) do |name, predicate|
-  model(name).should_not send("be_#{predicate.gsub(' ', '_')}")
+  model!(name).should_not send("be_#{predicate.gsub(' ', '_')}")
 end
