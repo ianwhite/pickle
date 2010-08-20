584e7c84940b0cd174d419207ac268cab0381f65
Ian White <ian.w.white@gmail.com>
2010-08-20 14:02:43 +0100

    Fix make_matcher_spec, I had wrongly assumed that be_empty != by_any (rspec matchers don't do equality the way I expected).  Rewritten in behavioural style

--- spec/pickle/make_matcher_spec.rb
+++ spec/pickle/make_matcher_spec.rb
@@ -1,31 +1,45 @@
 require 'spec_helper'
 
 describe Pickle::MakeMatcher do
-  it ".make_matcher(example, string) should create a matcher on demand using the string" do
-    matcher = Pickle::MakeMatcher.make_matcher(self, "empty")
-    matcher.should == be_empty
-  end
-  
-  describe "included into context" do
-    include Pickle::MakeMatcher
-
-    describe "context.make_matcher(string)" do
-      it "should create a matcher" do
-        make_matcher('empty').should == be_empty
-      end
+  shared_examples_for "be_empty matcher" do
+    describe "should match" do
+      specify("[]") { [].should subject }
+      specify("''") { "".should subject }
     end
     
-    describe "parsing input" do
-      it "'fairly large' should == be_fairly_large" do
-        make_matcher('fairly large').should == be_fairly_large
+    describe "should_not match" do
+      specify("[1]") { [1].should_not subject }
+      specify("'1'") { "1".should_not subject }
+    end
+  end
+  
+  describe ".make_matcher(example, 'empty')" do
+    subject { Pickle::MakeMatcher.make_matcher(self, "empty") }
+    it_should_behave_like "be_empty matcher"
+  end
+    
+  describe "(included)" do
+    include Pickle::MakeMatcher
+
+    describe "#make_matcher(string)" do
+      subject { make_matcher('empty') }
+      it_should_behave_like "be_empty matcher"
+    end
+    
+    describe "asking if something is #fairly_large? should work with" do
+      subject { mock }
+      before { subject.should_receive(:fairly_large?).and_return(true) }
+      
+      specify "'fairly large'" do
+        should make_matcher('fairly large')
       end
       
-      it "'\"fairly large\"' should == be_fairly_large" do
-        make_matcher('"fairly large"').should == be_fairly_large
+      specify "'\"fairly large\"'" do
+        should make_matcher('"fairly large"')
       end
 
-      it "'\"Fairly Large\"' should == be_fairly_large" do
-        make_matcher('"Fairly Large"').should == be_fairly_large
+      specify "'\"Fairly Large\"'" do
+        should make_matcher('"Fairly Large"')
       end
     end
   end
