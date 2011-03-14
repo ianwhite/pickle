7ab5f8267184702b6c70fcc19618421f43de7d1c
Ian White <ian.w.white@gmail.com>
2011-03-14 07:31:52 +0000

    Adds autotest stuff

---
+++ autotest/discover.rb
@@ -0,0 +1,7 @@
+Autotest.add_hook :initialize do |autotest|
+  %w{.git .DS_Store ._* vendor tmp email}.each do |exception|
+    autotest.add_exception(exception)
+  end
+end
+
+Autotest.add_discovery { "rspec2" }
