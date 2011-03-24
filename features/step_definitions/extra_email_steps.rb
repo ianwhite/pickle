b7ca24360385688adfa3f16685b9bb294f12add5
Ian White <ian.w.white@gmail.com>
2011-03-24 09:40:06 +0000

    Update features for cucumber 0.10.2 and cucumber-rails 0.4.0

--- features/step_definitions/extra_email_steps.rb
+++ features/step_definitions/extra_email_steps.rb
@@ -2,6 +2,11 @@ Given(/^an email "(.*?)" with body: "(.*?)" is delivered to (.+?)$/) do |subject
   Notifier.email(to, subject, body).deliver
 end
 
+Given(/^an email with a link "(.+?)" to (.+?) is delivered to (.+?)$/) do |text, page, to|
+  body = "some text <a href='http://example.com/#{path_to(page)}'>#{text}</a> more text" 
+  Notifier.email(to, "example", body).deliver
+end
+
 Given(/^#{capture_model}'s email is delivered$/) do |model|
   Notifier.user_email(model!(model)).deliver
 end
