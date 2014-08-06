cd2b5d741802fe301aac7ce211cf5c9cc1853858
James Le Cuirot <james.le-cuirot@yakara.com>
2014-08-06 22:06:54 +0100

    Fix features for latest factory_girl

--- features/app/factories.rb
+++ features/app/factories.rb
@@ -1,23 +1,25 @@
 # Factories
 require 'factory_girl'
 
-Factory.sequence :fork_name do |n|
-  "fork %d04" % n
-end
+FactoryGirl.define do
+  sequence :fork_name do |n|
+    "fork %d04" % n
+  end
 
-Factory.define :fork do |f|
-  f.name { Factory.next(:fork_name) }
-end
+  factory :fork do |f|
+    f.name { FactoryGirl.generate(:fork_name) }
+  end
 
-Factory.define :tine do |t|
-  t.association :fork
-end
+  factory :tine do |t|
+    t.association :fork
+  end
 
-Factory.define :rusty_tine, :class => Tine do |t|
-  t.association :fork
-  t.rusty true
-end
+  factory :rusty_tine, :class => Tine do |t|
+    t.association :fork
+    t.rusty true
+  end
 
-Factory.define :fancy_fork, :class => Fork do |t|
-  t.name { "Fancy " + Factory.next(:fork_name) }
+  factory :fancy_fork, :class => Fork do |t|
+    t.name { "Fancy " + FactoryGirl.generate(:fork_name) }
+  end
 end
