dff2de7d7aae031c94f56f5a34edaa9681b6c206
Kevin Ross <kevin.ross@alienfast.com>
2014-09-16 12:02:06 -0500

    Better email validation messages

--- rails_generators/pickle/templates/email_steps.rb
+++ rails_generators/pickle/templates/email_steps.rb
@@ -27,37 +27,39 @@ When(/^(?:I|they) click the first link in #{capture_email}$/) do |email_ref|
 end
 
 Then(/^(\d)+ emails? should be delivered to (.*)$/) do |count, to|
-  expect(emails("to: \"#{email_for(to)}\"").size).to eq(count.to_i)
+  actual = emails("to: \"#{email_for(to)}\"").size
+  expect(actual).to eq(count.to_i), "Expected #{count} emails but encountered #{actual} delivered to #{to}"
 end
 
 Then(/^(\d)+ emails? should be delivered with #{capture_fields}$/) do |count, fields|
-  expect(emails(fields).size).to eq(count.to_i)
+  actual = emails(fields).size
+  expect(actual).to eq(count.to_i), "Expected #{count} emails but encountered #{actual} to be delivered with #{fields}"
 end
 
 Then(/^#{capture_email} should be delivered to (.+)$/) do |email_ref, to|
-  expect(email(email_ref, "to: \"#{email_for(to)}\"")).not_to be_nil
+  expect(email(email_ref, "to: \"#{email_for(to)}\"")).not_to be_nil, "Failed to find #{email_ref} delivered to: #{to}"
 end
 
 Then(/^#{capture_email} should not be delivered to (.+)$/) do |email_ref, to|
-  expect(email(email_ref, "to: \"#{email_for(to)}\"")).to be_nil
+  expect(email(email_ref, "to: \"#{email_for(to)}\"")).to be_nil, "Unexpectedly found #{email_ref} delivered to: #{to}"
 end
 
 Then(/^#{capture_email} should have #{capture_fields}$/) do |email_ref, fields|
-  expect(email(email_ref, fields)).not_to be_nil
+  expect(email(email_ref, fields)).not_to be_nil, "Failed to find #{fields} in #{email_ref}"
 end
 
 Then(/^#{capture_email} should contain "(.*)"$/) do |email_ref, text|
-  expect(email(email_ref).body).to match(/#{text}/)
+  expect(email(email_ref).body).to match(/#{text}/), "Failed to find \"#{text}\" in #{email_ref}"
 end
 
 Then(/^#{capture_email} should not contain "(.*)"$/) do |email_ref, text|
-  expect(email(email_ref).body).not_to match(/#{text}/)
+  expect(email(email_ref).body).not_to match(/#{text}/), "Unexpectedly found \"#{text}\" in #{email_ref}"
 end
 
 Then(/^#{capture_email} should link to (.+)$/) do |email_ref, page|
-  expect(email(email_ref).body).to match(/#{path_to(page)}/)
+  expect(email(email_ref).body).to match(/#{path_to(page)}/), "Failed to find link to #{page} in #{email_ref}"
 end
 
 Then(/^show me the emails?$/) do
-   save_and_open_emails
+  save_and_open_emails
 end
