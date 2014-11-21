152313720bb98122b43eada6c38e304e46524b27
Matt Royal <mroyal@gmail.com>
2014-11-21 22:58:16 +0000

    The Mongoid adapter ignores classes instances without a name
    An anonymous class that includes Mongoid::Document is created in
    Mongoid::Config#destructive_fields, which previously caused the
    Mongoid adapter in Pickle to raise an exception in some cases.

--- lib/pickle/adapters/mongoid.rb
+++ lib/pickle/adapters/mongoid.rb
@@ -12,7 +12,9 @@ module Mongoid
 
       # Gets a list of the available models for this adapter
       def self.model_classes
-        ObjectSpace.each_object(Class).to_a.select {|klass| klass.ancestors.include? Mongoid::Document}
+        ObjectSpace.each_object(Class).to_a.select do |klass|
+          klass.name && klass.ancestors.include?(Mongoid::Document)
+        end
       end
 
       # get a list of column names for a given class
