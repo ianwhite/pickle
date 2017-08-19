38faa788fefddade4df64ffd77fbaf15c164c519
Mathieu Jobin <mathieu@venuenext.com>
2017-08-18 20:51:59 -0400

    fix email delivery deprecation notice

--- features/step_definitions/extra_email_steps.rb
+++ features/step_definitions/extra_email_steps.rb
@@ -1,12 +1,12 @@
 Given(/^an email "(.*?)" with body: "(.*?)" is delivered to (.+?)$/) do |subject, body, to|
-  Notifier.email(to, subject, body).deliver
+  Notifier.email(to, subject, body).deliver_now
 end
 
 Given(/^an email with a link "(.+?)" to (.+?) is delivered to (.+?)$/) do |text, page, to|
-  body = "some text <a href='http://example.com/#{path_to(page)}'>#{text}</a> more text" 
-  Notifier.email(to, "example", body).deliver
+  body = "some text <a href='http://example.com/#{path_to(page)}'>#{text}</a> more text"
+  Notifier.email(to, "example", body).deliver_now
 end
 
 Given(/^#{capture_model}'s email is delivered$/) do |model|
-  Notifier.user_email(model!(model)).deliver
+  Notifier.user_email(model!(model)).deliver_now
 end
